c1444ced3a6b65ac15240c736e0c1323f03b8b65
swethabura <swethabura01@gmail.com>
2025-04-16 18:58:16 +0530

    render api addition

--- server.js
+++ server.js
@@ -11,16 +11,16 @@ app.use(express.json());
 
 app.use("/api/transactions", transactionRoutes);
 
+const BASE_URL = process.env.PROCESS_URL 
+
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => {
-    app.listen(5000, () => console.log(`Server running on port http://localhost:5000`));
+    app.listen(5000, () => console.log(`Server running on port ${BASE_URL}`));
   })
   .catch((err) => console.error("MongoDB connection error:", err));
 
-// app.listen(5124, () => {
-//   console.log("Server is starting...");
-// });
+
